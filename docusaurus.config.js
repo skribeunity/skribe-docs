7ecdf209e4f817d2f52f6645b31e90459f29aaca
theDevJade <jadeisstolen@gmail.com>
2025-04-25 13:42:22 -0700

    Fix configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://skribeunity.github.io/skribe-docs',
+  url: 'https://skribeunity.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/skribe-docs',
+  baseUrl: '/skribe-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
